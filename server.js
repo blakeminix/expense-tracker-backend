c1e5f4ee9945bc4b13e399509ff591227baced04
blakeminix <blakeminix2@gmail.com>
2024-07-10 12:59:27 -0400

    MongoDB Integration

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let expensesCollection;
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    const database = client.db('ExpenseTracker');
+    expensesCollection = database.collection('expenses');
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+  }
+}
+
+connectToDatabase();
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -10,21 +35,21 @@ app.use(cors());
 app.post('/api/expenses', async (req, res) => {
   const { amount, note, category, date, userEmail } = req.body;
   const newExpense = {
-    amount: amount,
-    note: note,
-    category: category,
-    date: date,
-    userEmail: userEmail,
+    amount,
+    note,
+    category,
+    date,
+    userEmail,
   };
   try {
-    console.log(newExpense);
-    res.status(201).json(newExpense);
+    const result = await expensesCollection.insertOne(newExpense);
+    res.status(201).json({ message: 'Expense added', expense: newExpense });
   } catch (error) {
     res.status(400).json({ error: 'Failed to add expense' });
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
